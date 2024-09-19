d669415e6a5fd3957371be4b855f32f7e95e6f8d
UzielSolis <uziel.solis@iteso.mx>
2024-09-19 10:47:42 -0600

    angular material

--- my-dream-app/src/app/components/paths/movies/movie-list/movie-list.component.ts
+++ my-dream-app/src/app/components/paths/movies/movie-list/movie-list.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../modules/material/material.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, FormsModule],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss'
 })
 export class MovieListComponent {
+
+  submit() {
+    console.log('Form submitted');
+  }
 
 }
---
+++ my-dream-app/src/app/modules/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
+})
+export class MaterialModule { }
