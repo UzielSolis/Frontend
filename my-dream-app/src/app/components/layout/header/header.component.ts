33f06233fe8db6e1bd8d11a5dc1fb44e02af5eae
UzielSolis <uziel.solis@iteso.mx>
2024-10-22 10:51:21 -0600

    services

--- my-dream-app/src/app/components/layout/header/header.component.ts
+++ my-dream-app/src/app/components/layout/header/header.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../modules/material/material.module';
-
+import { PageService } from '../../../services/page.service';
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule, MaterialModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
+  counter: number = 0;
+
+  constructor(pageService: PageService) {
+    pageService.counterObs.subscribe((counter: number) => {
+      this.counter = counter;
+    });
+  }
+  
 }
--- my-dream-app/src/app/components/paths/home/home.component.ts
+++ my-dream-app/src/app/components/paths/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PageService } from '../../../services/page.service';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  constructor(private pageService: PageService) { }
+
+  counter: number = 0;
+
+  increaseCounter() {
+    this.pageService.increaseCounter();
+  }
 
 }
